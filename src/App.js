1b79fc65238e74cdccb5c27c2c0cf663a52e1199
Illia Denisov <robbinnegood@gmail.com>
2025-12-19 18:55:55 +0300

    Add ascending/descending order toggle button

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [currentMove, setCurrentMove] = useState(0);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [isSortedinAscendingOrder, setIsSortedinAscendingOrder] = useState(true);
   const currentSquares = history[currentMove];
   const xIsNext = currentMove % 2 === 0;
 
@@ -96,7 +97,8 @@ export default function Game() {
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <button className="sort-order-button" onClick={() => setIsSortedinAscendingOrder(!isSortedinAscendingOrder)}>{isSortedinAscendingOrder ? "Sort in descending order" :  "Sort in ascending order"} </button>
+        <ol>{isSortedinAscendingOrder ? moves : moves.reverse()}</ol>
       </div>
     </div>
   )
